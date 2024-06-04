99ff7660c8a48e1f1b2a908619f3f124486cc757
송이현 <songehyun00@gmail.com>
2024-06-04 11:47:57 +0900

    this 햇갈릴 때 보기

--- work-3.js
+++ work-3.js
@@ -12,6 +12,10 @@ let itsMe = {
   age: 31,
   home: "대전",
   test: this,
+  sentence: function () {
+    return this.name + "은 미남이다.";
+  },
 };
 
 console.dir("객체에서 보는 this :", itsMe);
+console.log("메서드로 보는 this :", itsMe.sentence());
