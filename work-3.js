aeac9bf22db1153214b37f496f9f145486096b63
송이현 <songehyun00@gmail.com>
2024-06-04 11:40:58 +0900

    iteMe 추가

--- work-3.js
+++ work-3.js
@@ -6,3 +6,12 @@ function workThree(a, b) {
 }
 
 console.log(workThree(1, 2));
+
+let itsMe = {
+  name: "송이현",
+  age: 31,
+  home: "대전",
+  test: this,
+};
+
+console.dir("객체에서 보는 this :", itsMe);
